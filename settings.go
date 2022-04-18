d08df26ea01658d3a6d106266f3cd669a7e3e5d2
Andrew Dunai <a@dun.ai>
2022-04-18 13:20:40 +0300

    Load settings from file

--- settings.go
+++ settings.go
@@ -10,56 +10,46 @@ import (
 )
 
 type Settings struct {
-	TimezoneName string `env:"TZ" envDefault:"Europe/Kiev"`
+	TimezoneName string `env:"TZ" envDefault:"Europe/Kiev" yaml:"timezone_name"`
 	Timezone     *time.Location
-	APIKeys      []string `env:"API_KEYS" envSeparator:"," envDefault:""`
-	APIKeysFile  string   `env:"API_KEYS_FILE" envDefault:""`
-	Debug        bool     `env:"DEBUG" envDefault:"false"`
-	BacklogSize  int      `env:"BACKLOG_SIZE" envDefault:"200"`
+	APIKeys      []string `env:"API_KEYS" envSeparator:"," envDefault:"" yaml:"api_keys"`
+	Debug        bool     `env:"DEBUG" envDefault:"false" yaml:"debug"`
+	BacklogSize  int      `env:"BACKLOG_SIZE" envDefault:"200" yaml:"backlog_size"`
 }
 
 func MustLoadSettings() (settings Settings) {
 	var err error
 
-	opts := env.Options{
-		RequiredIfNoDef: true,
-		OnSet: func(tag string, value interface{}, isDefault bool) {
-			if isDefault {
-				log.Warnf("settings: using default value for env var %s: %s", tag, value)
-			}
-		},
-	}
+	settings.TimezoneName = "Europe/Kiev"
 
-	if err = env.Parse(&settings, opts); err != nil {
-		log.Fatalf("settings: load: %s", err)
+	if len(os.Args) > 1 {
+		var f *os.File
+
+		f, err = os.Open(os.Args[1])
+		if err != nil {
+			log.Fatalf("settings: open settings file: %s", err)
+		}
+
+		dec := yaml.NewDecoder(f)
+		if err = dec.Decode(&settings); err != nil {
+			log.Fatalf("settings: load settings from file: %s", err)
+		}
+	} else {
+		opts := env.Options{
+			RequiredIfNoDef: true,
+			OnSet: func(tag string, value interface{}, isDefault bool) {
+				if isDefault {
+					log.Warnf("settings: using default value for env var %s: %s", tag, value)
+				}
+			},
+		}
+		if err = env.Parse(&settings, opts); err != nil {
+			log.Fatalf("settings: load: %s", err)
+		}
 	}
 
 	if settings.Timezone, err = time.LoadLocation(settings.TimezoneName); err != nil {
 		log.Fatalf("settings: load timezone: %s", err)
-	}
-
-	if len(settings.APIKeys) > 0 && settings.APIKeysFile != "" {
-		log.Fatalf("settings: cannot provide both API_KEYS and API_KEYS_FILE, choose one")
-	}
-
-	var keysData struct {
-		APIKeys []string `yaml:"keys"`
-	}
-
-	if settings.APIKeysFile != "" {
-		var f *os.File
-
-		f, err = os.Open(settings.APIKeysFile)
-		if err != nil {
-			log.Fatalf("settings: open API keys file: %s", err)
-		}
-
-		dec := yaml.NewDecoder(f)
-		if err = dec.Decode(&keysData); err != nil {
-			log.Fatalf("settings: load API keys from file: %s", err)
-		}
-
-		settings.APIKeys = keysData.APIKeys
 	}
 
 	if len(settings.APIKeys) == 0 {
@@ -67,6 +57,7 @@ func MustLoadSettings() (settings Settings) {
 	}
 
 	log.Infof("settings: load %d API keys", len(settings.APIKeys))
+	log.Infof("settings: %v", settings)
 
 	return
 }
